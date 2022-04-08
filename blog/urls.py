b7c67350dec7d575f5a7417222d43fb405dd484d
SooHwan Kim <newpoo@naver.com>
2022-04-08 17:42:14 +0900

    post에 tag추가, 태그 뷰 생성

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.PostList.as_view()),
     #path('', views.index),
     #path('<int:pk>/', views.single_post_page),
-    path('category/<str:slug>/', views.show_category_posts)
+    path('category/<str:slug>/', views.show_category_posts),
+    path('tag/<str:slug>/', views.show_tag_posts)
 ]
